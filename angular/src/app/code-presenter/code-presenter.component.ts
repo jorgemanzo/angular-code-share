86998c6ebea19d4c3eb949038837ef0554f7d82f
jorgemanzo <jmanzo203689@gmail.com>
2020-08-16 19:39:32 -0700

    Init presenter by route ID

--- angular/src/app/code-presenter/code-presenter.component.ts
+++ angular/src/app/code-presenter/code-presenter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SharesService } from '../shares.service';
+import { Share } from '../share';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-code-presenter',
@@ -7,12 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodePresenterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private sharesService: SharesService,
+    private route: ActivatedRoute
+  ) { }
 
   mutable: boolean = false;
   code: string = "code from api;";
 
   ngOnInit(): void {
+    this.initByRouteParams();
   }
 
+  initByRouteParams(): void {
+    const routeId = +this.route.snapshot.paramMap.get('id')
+    if(routeId > 0) {
+      this.sharesService.getShareById(routeId).subscribe(
+        res => {
+          this.mutable = res[0].mutable == "0" ? false : true;
+          this.code = res[0].code;
+        }
+      )
+    }
+  }
 }
