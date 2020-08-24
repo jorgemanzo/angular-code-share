d5d0af5694101ac6a311e2d9255c4d2c886877ff
jorgemanzo <jmanzo203689@gmail.com>
2020-08-24 14:53:09 -0700

    Angular service posts to go api

--- go/api.go
+++ go/api.go
@@ -1,24 +1,38 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"net/http"
+	// _ "github.com/go-sql-driver/mysql"
 )
 
 type Share struct {
-    Code string `json:"code"`
-    Mutable bool `json:"mutable"`
+	Code    string `json:"code"`
+	Mutable bool   `json:"mutable"`
+}
+
+func allowCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 }
 
 func main() {
-    http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
-        share := Share {
-            Code: "printf()",
-            Mutable: false,
-        }
-        json.NewEncoder(w).Encode(share)
-    })
+	// db, err := sql.Open("mysql", "admin:password@(127.0.0.1:3306)/code_share")
 
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		// log.Fatal(r.FormValue("code"))
+		share := Share{
+			Code:    "printf()",
+			Mutable: false,
+		}
+		allowCORS(w)
+		json.NewEncoder(w).Encode(share)
+	})
+
+	http.ListenAndServe(":8081", nil)
+
 }
--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -2,8 +2,8 @@
 
 service mysql start
 mysql < /root/init.sql
-cd /var/www/html/go/
-go build -o build/ api.go
-./build/api &
+# cd /var/www/html/go/
+# go build -o build/ api.go
+# ./build/api &
 cd /var/www/html/angular/
 ng serve --host 0.0.0.0 --port 8080
