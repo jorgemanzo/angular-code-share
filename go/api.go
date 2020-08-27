bf7c3965610e05b34b00dc1e08471dc4ff45774a
jorgemanzo <jmanzo203689@gmail.com>
2020-08-26 21:35:18 -0700

    refactoring

--- go/api.go
+++ go/api.go
@@ -4,13 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Share struct {
+type share struct {
 	Code    string `json:"code"`
 	Mutable bool   `json:"mutable"`
 }
@@ -21,43 +20,41 @@ func setHeaders(w http.ResponseWriter, contentType string) {
 	w.Header().Set("Content-Type", contentType)
 }
 
+const mySQLInfo = "admin:password@(127.0.0.1:3306)/code_share"
+
+func getByID(w http.ResponseWriter, r *http.Request) {
+	const stmt = "SELECT code, mutable FROM shares WHERE share_id = ?"
+	snip := share{"", false}
+
+	db, _ := sql.Open("mysql", mySQLInfo)
+	db.QueryRow(stmt, r.URL.Query().Get("id")).Scan(&(snip.Code), &(snip.Mutable))
+	db.Close()
+
+	setHeaders(w, "application/json")
+	json.NewEncoder(w).Encode(snip)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	const stmt = "INSERT shares (code, mutable) VALUES ( ?, ?)"
+	mutability := false
+	if r.PostFormValue("mutable") == "1" {
+		mutability = true
+	}
+
+	db, _ := sql.Open("mysql", mySQLInfo)
+	result, _ := db.Exec(stmt, r.PostFormValue("code"), mutability)
+	db.Close()
+
+	shareID, _ := result.LastInsertId()
+	setHeaders(w, "text/plain")
+	fmt.Fprintf(w, "%d", shareID)
+}
+
 func main() {
 
-	http.HandleFunc("/get_by_id", func(w http.ResponseWriter, r *http.Request) {
-		db, _ := sql.Open("mysql", "admin:password@(127.0.0.1:3306)/code_share")
+	http.HandleFunc("/get_by_id", getByID)
 
-		const sql string = "SELECT code, mutable FROM shares WHERE share_id = ?"
-		snip := Share{"", false}
-		db.QueryRow(sql, r.URL.Query().Get("id")).Scan(&(snip.Code), &(snip.Mutable))
-
-		db.Close()
-
-		log.Printf("{%s, %v}\n", snip.Code, snip.Mutable)
-		setHeaders(w, "application/json")
-		json.NewEncoder(w).Encode(snip)
-	})
-
-	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("mysql", "admin:password@(127.0.0.1:3306)/code_share")
-
-		if err != nil {
-			panic(err)
-		}
-
-		mutability := false
-		if r.PostFormValue("mutable") == "1" {
-			mutability = true
-		}
-
-		sql := "INSERT shares (code, mutable) VALUES ( ?, ?)"
-		result, _ := db.Exec(sql, r.PostFormValue("code"), mutability)
-		shareID, _ := result.LastInsertId()
-
-		db.Close()
-
-		setHeaders(w, "text/plain")
-		fmt.Fprintf(w, "%d", shareID)
-	})
+	http.HandleFunc("/create", create)
 
 	http.ListenAndServe(":8081", nil)
 
